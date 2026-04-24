0bd1a07508326bd9a5f55a3a08cb5990217408aa
Roberta-26 <robertarodriguesroro16@gmail.com>
2026-04-23 21:03:53 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const API_KEY = "ghp_xK92mNpL34rTvQ87wZaB56cDeFgHiJkL";
-const DB_PASSWORD = "admin@prod#2024";
+const DB_PASSWORD = "SENHA_BANCO_DE_DADOS";
 const API_KEY = "process.env.API_KEY";
 const DB_PASSWORD = "process.env.DB_PASSWORD";
 
